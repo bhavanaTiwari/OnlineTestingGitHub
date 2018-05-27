5704028de39fa81f4a073b800acdbdf6752a7242
bhavana tiwari <bhavana.srh@gmail.com>
2018-05-28 00:11:43 +0200

    first checkin to test GIT

---
+++ src/GitTest/TestGitClass.java
@@ -0,0 +1,24 @@
+package GitTest;
+
+public class TestGitClass {
+
+	public static void main(String[] args) {
+		           
+		     int x = sum();
+		     System.out.println("test git  " + x);  
+		          
+			}
+
+			private static int sum() {
+				
+				int sum =2;int i =0;		
+				for( i =0 ; i<5; i++)
+				{
+					sum = sum + i;
+				}
+				return sum;
+			}
+
+	}
+
+
